e2223d41a7370c188f5afd9556558bb1fbad5e1c
Sam Gerstner <66745349+samgerstner@users.noreply.github.com>
2025-07-25 11:01:17 -0700

    Initial Commit

---
+++ src/main/java/pro/samgerstner/licensexpress/LicensexpressApplication.java
@@ -0,0 +1,13 @@
+package pro.samgerstner.licensexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicensexpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicensexpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/samgerstner/licensexpress/LicensexpressApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.samgerstner.licensexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensexpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
