fe848873b9caf9b92aaa1f3c03937f99609f6671
Sam Gerstner <66745349+samgerstner@users.noreply.github.com>
2025-08-01 10:05:27 -0700

    Add missing imports

--- src/main/java/pro/samgerstner/licensexpress/security/ApplicationUserDetailsService.java
+++ src/main/java/pro/samgerstner/licensexpress/security/ApplicationUserDetailsService.java
@@ -2,9 +2,14 @@ package pro.samgerstner.licensexpress.security;
 
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import pro.samgerstner.licensexpress.entities.ApplicationUser;
+import pro.samgerstner.licensexpress.entities.repositories.ApplicationUserRepository;
 
 @Service
 @Transactional
